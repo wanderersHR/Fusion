6ffc105863fc5a9e4ae01063d6b6795892520ba7
Corné den Breejen <cornedev@outlook.com>
2022-03-30 11:34:11 +0200

    Create firebaseauth.js

---
+++ firebaseauth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcVMI_fHeXfiC-fyRAHSR_xeRSqj7Hjvs",
+  authDomain: "fusion-da358.firebaseapp.com",
+  projectId: "fusion-da358",
+  storageBucket: "fusion-da358.appspot.com",
+  messagingSenderId: "961221085493",
+  appId: "1:961221085493:web:f963ac2df44add0175247c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
