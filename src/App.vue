01fcc0b596b05657cf2b580f1109fd9292848e60
Corné den Breejen <cornedev@outlook.com>
2022-04-19 11:24:34 +0200

    Added firebase and cloud functions support

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,48 @@
 <!-- @format -->
 
 <template>
+	<p>Start: {{ startTime }}</p>
+	<p>End: {{ endTime }}</p>
+	<p>Duration: {{ duration }}</p>
+	<p>Response: {{ firebaseMsg }}</p>
+
 	<router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useFirebaseStore } from "./stores/firebase";
+import { httpsCallable } from "firebase/functions";
+import { ref } from "vue";
+
+export default defineComponent({
+	setup() {
+		const startTime = ref(Date.now());
+		let duration = ref(-1);
+		let endTime = ref(-1);
+		let firebaseMsg = ref("");
+
+		const firebaseStore = useFirebaseStore();
+
+		firebaseStore.loadFirebase();
+
+		const functions = firebaseStore.functions;
+
+		// connectFunctionsEmulator(functions, "localhost", 5001);
+		const addMessage = httpsCallable(functions, "helloWorld");
+		addMessage().then((result) => {
+			// Read result of the Cloud Function.
+			/** @type {any} */
+			const data = result.data;
+			console.log(data);
+
+			firebaseMsg.value = data as string;
+
+			endTime.value = Date.now();
+			duration.value = endTime.value - startTime.value;
+		});
+
+		return { startTime, duration, endTime, firebaseMsg };
+	},
+});
+</script>
