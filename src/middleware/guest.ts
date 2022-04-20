ee8432e82430ba0bc3dfab198a676b9fdddbea14
Corné den Breejen <cornedev@outlook.com>
2022-04-20 12:34:16 +0200

    Added middleware for protected and guest routes

--- src/middleware/guest.ts
+++ src/middleware/guest.ts
@@ -2,12 +2,12 @@
 
 import { useAuthenticationStore } from "../stores/authentication";
 
-export default function auth(to, from, next) {
+export default function guest(to, from, next) {
 	const authenticationStore = useAuthenticationStore();
 
 	if (authenticationStore.isAuthenticated) {
-		return next();
+		return next({ name: "home" });
 	}
 
-	return next({ name: "login" });
+	return next();
 }
