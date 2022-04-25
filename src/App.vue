b67983204dfa6317e3bff0a2a9a0401a5b0e4092
Corné den Breejen <cornedev@outlook.com>
2022-04-25 15:09:31 +0200

    Added cloud functions test material

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <!-- @format -->
 
 <template>
-	<p>Start: {{ startTime }}</p>
-	<p>End: {{ endTime }}</p>
-	<p>Duration: {{ duration }}</p>
-	<p>Response: {{ firebaseMsg }}</p>
-
 	<router-view />
 </template>
 
@@ -13,36 +8,20 @@
 import { defineComponent } from "vue";
 import { useFirebaseStore } from "./stores/firebase";
 import { httpsCallable } from "firebase/functions";
-import { ref } from "vue";
 
 export default defineComponent({
 	setup() {
-		const startTime = ref(Date.now());
-		let duration = ref(-1);
-		let endTime = ref(-1);
-		let firebaseMsg = ref("");
-
 		const firebaseStore = useFirebaseStore();
 
 		firebaseStore.loadFirebase();
 
 		const functions = firebaseStore.functions;
 
-		// connectFunctionsEmulator(functions, "localhost", 5001);
-		const addMessage = httpsCallable(functions, "helloWorld");
-		addMessage().then((result) => {
-			// Read result of the Cloud Function.
-			/** @type {any} */
-			const data = result.data;
+		const hoursOfSingleTicket = httpsCallable(functions, "getHoursByTicket");
+		hoursOfSingleTicket({ ticket: "bs-2" }).then((result) => {
+			const { data } = result;
 			console.log(data);
-
-			firebaseMsg.value = data as string;
-
-			endTime.value = Date.now();
-			duration.value = endTime.value - startTime.value;
 		});
-
-		return { startTime, duration, endTime, firebaseMsg };
 	},
 });
 </script>
