86f1a4f2a81bd67dd79fa90f8bfc64b5fa7bde30
Foxxite <foxxite@foxxite.com>
2022-05-17 10:46:22 +0200

    Jira Scopes + Read projects cloud function

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
 	<p>Duration: {{ duration }}</p>
 	<p>Response: {{ firebaseMsg }}</p> -->
 
-
 	<router-view />
 </template>
 
@@ -17,37 +16,40 @@ import { httpsCallable } from "firebase/functions";
 import { ref } from "vue";
 import { useAuthenticationStore } from "./stores/authentication";
 
-export default defineComponent({
-	setup() {
-		// const startTime = ref(Date.now());
-		// let duration = ref(-1);
-		// let endTime = ref(-1);
-		// let firebaseMsg = ref("");
-		// const firebaseStore = useFirebaseStore();
-		// firebaseStore.loadFirebase();
-		// const functions = firebaseStore.functions;
-		// // connectFunctionsEmulator(functions, "localhost", 5001);
-		// const addMessage = httpsCallable(functions, "helloWorld");
-		// addMessage().then((result) => {
-		// 	// Read result of the Cloud Function.
-		// 	/** @type {any} */
-		// 	const data = result.data;
-		// 	console.log(data);
-		// 	firebaseMsg.value = data as string;
-		// 	endTime.value = Date.now();
-		// 	duration.value = endTime.value - startTime.value;
-		// });
-		// return { startTime, duration, endTime, firebaseMsg };
+// export default defineComponent({
+// 	setup() {
+// 		// const startTime = ref(Date.now());
+// 		// let duration = ref(-1);
+// 		// let endTime = ref(-1);
+// 		// let firebaseMsg = ref("");
+// 		// const firebaseStore = useFirebaseStore();
+// 		// firebaseStore.loadFirebase();
+// 		// const functions = firebaseStore.functions;
+// 		// // connectFunctionsEmulator(functions, "localhost", 5001);
+// 		// const addMessage = httpsCallable(functions, "helloWorld");
+// 		// addMessage().then((result) => {
+// 		// 	// Read result of the Cloud Function.
+// 		// 	/** @type {any} */
+// 		// 	const data = result.data;
+// 		// 	console.log(data);
+// 		// 	firebaseMsg.value = data as string;
+// 		// 	endTime.value = Date.now();
+// 		// 	duration.value = endTime.value - startTime.value;
+// 		// });
+// 		// return { startTime, duration, endTime, firebaseMsg };
+// 	}
 
-		onBeforeMount(() => {
-			const authenticationStore = useAuthenticationStore();
-			authenticationStore.fetchAccessToken();
-			if (authenticationStore.isAuthenticated) {
-				setInterval(() => {
-					authenticationStore.refresh();
-					console.log("hoi");
-				}, 10 * 1000);
-			}
+// 		onBeforeMount(() => {
+// 			const authenticationStore = useAuthenticationStore();
+// 			authenticationStore.fetchAccessToken();
+// 			if (authenticationStore.isAuthenticated) {
+// 				setInterval(() => {
+// 					authenticationStore.refresh();
+// 					console.log("hoi");
+// 				}, 10 * 1000);
+// 			}
+
+// });
 
 export default defineComponent({
 	setup() {
--- src/views/Protected.vue
+++ src/views/Protected.vue
@@ -3,3 +3,32 @@
 <template>
 	<h1>Protected area</h1>
 </template>
+
+<script lang="ts">
+import { httpsCallable } from "firebase/functions";
+import { defineComponent, onBeforeMount } from "vue";
+import { useAuthenticationStore } from "../stores/authentication";
+import { useFirebaseStore } from "../stores/firebase";
+
+export default defineComponent({
+	setup() {},
+
+	onMounted() {
+		console.log("Protected.vue mounted");
+
+		const firebaseStore = useFirebaseStore();
+
+		firebaseStore.loadFirebase();
+
+		const authenticationStore = useAuthenticationStore();
+
+		const functions = firebaseStore.functions;
+
+		const projects = httpsCallable(functions, "getProjects");
+		projects({ bearerToken: authenticationStore.getBearerToken }).then((result) => {
+			const { data } = result;
+			console.log(data);
+		});
+	},
+});
+</script>
