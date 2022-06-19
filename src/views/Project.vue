651426b9066fbab0488d4d2b6820a675222df994
Corné den Breejen <cornedev@outlook.com>
2022-06-19 19:19:08 +0200

    Added month filter

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -2,7 +2,8 @@
 <template>
 	<Navigation />
 	<h1 style="text-align: center">Tickets for Project {{ projectName }}</h1>
-	<div v-if="issues.length > 0">
+	<Datepicker v-model="picked" monthPicker />
+	<div v-if="filteredIssues.length > 0">
 		<div class="main-box">
 			<div class="ticket-columns">
 				<IssueComponent v-for="issue in filteredIssues" v-bind:key="issue.id" v-bind:issue="issue" />
@@ -35,10 +36,12 @@ import { useRoute } from "vue-router";
 import Navigation from "../components/Navigation.vue";
 import { useFirebaseStore } from "../stores/firebase";
 import { Issue, JiraProjectDetails } from "../JiraResponses/JiraProjectDetail";
-
 import Loader from "../components/Loader.vue";
 import IssueComponent from "../components/Issue.vue";
 import { useAuthenticationStore } from "../stores/authentication";
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+import moment from "moment";
 
 export default defineComponent({
 	setup() {
@@ -47,9 +50,19 @@ export default defineComponent({
 		const route = useRoute();
 		const projectName = route.params.name;
 		const issues = ref<Issue[]>([]);
+		const picked = ref({
+			month: new Date().getMonth(),
+			year: new Date().getFullYear(),
+		});
 
 		const filteredIssues = computed(() => {
-			return issues.value.filter((issue) => issue.fields.creator.accountId === authorId);
+			let ownTickets = issues.value.filter((issue) => issue.fields.creator.accountId === authorId);
+			let monthTickets = ownTickets.filter(
+				(issue) =>
+					moment(issue.fields.created).month() === picked.value.month &&
+					moment(issue.fields.created).year() === picked.value.year
+			);
+			return monthTickets;
 		});
 
 		onMounted(() => {
@@ -67,8 +80,8 @@ export default defineComponent({
 			});
 		});
 
-		return { projectName, filteredIssues };
+		return { projectName, filteredIssues, picked };
 	},
-	components: { Loader, IssueComponent, Navigation },
+	components: { Loader, IssueComponent, Navigation, Datepicker },
 });
 </script>
