9dfbf66078665b0f1c58f17ef4077169d7c64fb7
Foxxite <foxxite@foxxite.com>
2022-06-21 17:21:55 +0200

    Refreshsknop

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ import App from "./App.vue";
 import router from "./router";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faJira } from "@fortawesome/free-brands-svg-icons";
-import { faArrowRightFromBracket, faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRightFromBracket, faCircleUser, faRotate } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import "./style sass/style.scss";
 
-library.add(faJira, faArrowRightFromBracket, faCircleUser);
+library.add(faJira, faArrowRightFromBracket, faCircleUser, faRotate);
 
 createApp(App).use(router).use(createPinia()).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
